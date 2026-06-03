a068a810c2f6c3abc9b579d85467bb8c48e6d6b4
AntonNesterchuk <39776539+tpAnswer4yourself@users.noreply.github.com>
2026-06-03 16:51:58 +0300

    Add files via upload

---
+++ code/frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
+import ProductDetails from './pages/ProductDetails';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
